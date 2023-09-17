aa324915147d8b69adf1ab9a7b75c31ab0c17002
Sen <alvrtsyan.seno@gmail.com>
2023-09-17 11:59:53 +0400

    isPowerOfThree | c++/typescript

---
+++ Typescript/isPowerOfThree.ts
@@ -0,0 +1,14 @@
+function isPowerOfThree(n: number): boolean {
+    if (n <= 0) {
+        return false;
+    }
+    
+    while (n > 1) {
+        if (n % 3 !== 0) {
+            return false;
+        }
+        n = Math.floor(n / 3);
+    }
+    
+    return n === 1;
+}
